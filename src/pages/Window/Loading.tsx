98b2b5494e180161618a5cd90754f771cf862224
Charles Chua <chuacharlesbon@gmail.com>
2023-05-18 22:02:03 +0800

    Updated: Loading Page

--- src/pages/Window/Loading.tsx
+++ src/pages/Window/Loading.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
+import { Div, FlexRow } from '../../core/Containers';
+import { ImSpinner2 } from 'react-icons/im';
 
 export const Loading: FC<any> = () => {
 
-const text = "Loading";
-
-return (
-    <h1>
-        {text}
-    </h1>
-);
+    return (
+        <Div className='fixed top-0 left-0 right-0 bottom-0 z-20 h-screen w-screen flex justify-center items-center'>
+            <FlexRow className='bg-grey-400 rounded-xl justify-center items-center w-24 h-24'>
+                <ImSpinner2 className="text-red-400 animate-spin text-4xl" />
+            </FlexRow>
+        </Div>
+    );
 };
